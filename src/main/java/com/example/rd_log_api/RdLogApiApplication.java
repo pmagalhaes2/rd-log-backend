2143812a29f25a17884ee9a5f4f91c8156001208
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-05-07 21:56:12 -0300

    chore: add basic configuration

---
+++ src/main/java/com/example/rd_log_api/RdLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rd_log_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdLogApiApplication.class, args);
+	}
+
+}
