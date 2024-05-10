11b40d903f8f1b04b2c0d295c32920db6a53be4d
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-05-10 18:21:47 -0300

    chore: create logistic company dto

---
+++ src/main/java/com/example/rd_log_api/domain/dto/requests/LogisticCompanyDto.java
@@ -0,0 +1,32 @@
+package com.example.rd_log_api.domain.dto.requests;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties
+public class LogisticCompanyDto {
+    private Long id;
+
+    private String name;
+
+    private String cnpj;
+
+    private Time opening_hours;
+
+    private Time closing_hours;
+
+    private String phone_number;
+
+    private String email;
+
+    private Boolean accepts_dangerous_loads;
+}
--- src/main/java/com/example/rd_log_api/domain/mappers/LogisticCompanyMapper.java
+++ src/main/java/com/example/rd_log_api/domain/mappers/LogisticCompanyMapper.java
@@ -1,11 +1,12 @@
 package com.example.rd_log_api.domain.mappers;
 
 import com.example.rd_log_api.domain.dto.requests.LogisticCompanyCreationRequest;
+import com.example.rd_log_api.domain.dto.requests.LogisticCompanyDto;
 import com.example.rd_log_api.domain.entities.LogisticCompany;
 
 
 public class LogisticCompanyMapper {
-    public static LogisticCompany toEntity(LogisticCompanyCreationRequest request) {
+    public static LogisticCompany toEntityFromCreationRequest(LogisticCompanyCreationRequest request) {
         return new LogisticCompany(
                 request.getId(),
                 request.getName(),
@@ -18,7 +19,20 @@ public class LogisticCompanyMapper {
         );
     }
 
-    public static LogisticCompanyCreationRequest toDto(LogisticCompany entity) {
+    public static LogisticCompany toEntityFromLogisticDto(LogisticCompanyDto dto) {
+        return new LogisticCompany(
+                dto.getId(),
+                dto.getName(),
+                dto.getCnpj(),
+                dto.getOpening_hours(),
+                dto.getClosing_hours(),
+                dto.getPhone_number(),
+                dto.getEmail(),
+                dto.getAccepts_dangerous_loads()
+        );
+    }
+
+    public static LogisticCompanyCreationRequest toCreationRequestDto(LogisticCompany entity) {
         return new LogisticCompanyCreationRequest(
                 entity.getId(),
                 entity.getName(),
@@ -30,4 +44,18 @@ public class LogisticCompanyMapper {
                 entity.getAccepts_dangerous_loads()
         );
     }
+
+    public static LogisticCompanyDto toLogisticDto(LogisticCompany entity) {
+        return new LogisticCompanyDto(
+                entity.getId(),
+                entity.getName(),
+                entity.getCnpj(),
+                entity.getOpening_hours(),
+                entity.getClosing_hours(),
+                entity.getPhone_number(),
+                entity.getEmail(),
+                entity.getAccepts_dangerous_loads()
+        );
+    }
+
 }
