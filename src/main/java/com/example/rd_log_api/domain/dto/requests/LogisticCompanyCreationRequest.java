8b952d2cf94d393a7374d1617523e27f30c5e893
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-05-10 17:29:42 -0300

    feat: create get by id and gell all to logistic companies

--- src/main/java/com/example/rd_log_api/domain/dto/requests/LogisticCompanyCreationRequest.java
+++ src/main/java/com/example/rd_log_api/domain/dto/requests/LogisticCompanyCreationRequest.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Time;
 
--- src/main/java/com/example/rd_log_api/service/LogisticCompanyService.java
+++ src/main/java/com/example/rd_log_api/service/LogisticCompanyService.java
@@ -4,14 +4,25 @@ import com.example.rd_log_api.domain.dto.requests.LogisticCompanyCreationRequest
 import com.example.rd_log_api.domain.entities.LogisticCompany;
 import com.example.rd_log_api.domain.mappers.LogisticCompanyMapper;
 import com.example.rd_log_api.repositories.LogisticCompanyRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LogisticCompanyService {
 
     @Autowired
     private LogisticCompanyRepository repository;
+
+    public List<LogisticCompanyCreationRequest> getAll() {
+        return repository.findAll().stream().map(LogisticCompanyMapper::toDto).toList();
+    }
+
+    public LogisticCompany getById(Long id){
+        return repository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
 
     public LogisticCompanyCreationRequest createLogisticCompany(LogisticCompanyCreationRequest logisticCompany) {
         LogisticCompany newLogisticCompany = LogisticCompanyMapper.toEntity(logisticCompany);
