65d21d9575c6c310afabbfd94a55521433a53af8
kurodrigues <katherineuchoas@icloud.com>
2024-05-14 13:52:52 -0300

    Criação de tabelas tipo de entrega, pedido cliente, entrega cliente, pedido suprimento e entrega suprimento.

--- src/main/resources/db.migration/v18__create-table-delivery_type.sql
+++ src/main/resources/db.migration/v18__create-table-delivery_type.sql
@@ -1,5 +1,5 @@
 CREATE TABLE delivery_type (
-                               id SERIAL PRIMARY KEY,
-                               size VARCHAR(50) NOT NULL, -- Tamanho da carga (pequeno, médio, grande, etc.)
-                               vehicle_type VARCHAR(50) NOT NULL -- Tipo de veículo (MOTO, CARRO, CAMINHÃO)
+    id SERIAL PRIMARY KEY,
+    size VARCHAR(50) NOT NULL, -- Tamanho da carga (pequeno, médio, grande, etc.)
+    vehicle_type VARCHAR(50) NOT NULL -- Tipo de veículo (MOTO, CARRO, CAMINHÃO)
 );
--- src/main/resources/db.migration/v24__create-table-supplies_delivery.sql
+++ src/main/resources/db.migration/v24__create-table-supplies_delivery.sql
@@ -1,11 +1,11 @@
 CREATE TABLE supplies_delivery(
-                                  id SERIAL PRIMARY KEY,
-                                  order_supplies_id INT NOT NULL,
-                                  volum DECIMAL (10,2) NOT NULL, -- No pedido já existe essa informação. Precisa repetir?
-                                  delivery_time TIMESTAMP NOT NULL, --DATETIME não foi aceito
-                                  logistic_companies_id INT NOT NULL,
-                                  status_id INT NOT NULL,
-                                  FOREIGN KEY (order_supplies_id) REFERENCES Order_supplies(id),
-                                  FOREIGN KEY (logistic_companies_id) REFERENCES Logistic_companies(id),
-                                  FOREIGN KEY (status_id) REFERENCES Status(id)
+    id SERIAL PRIMARY KEY,
+    order_supplies_id INT NOT NULL,
+    volum DECIMAL (10,2) NOT NULL, -- No pedido já existe essa informação. Precisa repetir?
+    delivery_time TIMESTAMP NOT NULL, --DATETIME não foi aceito
+    logistic_companies_id INT NOT NULL,
+    status_id INT NOT NULL,
+    FOREIGN KEY (order_supplies_id) REFERENCES Order_supplies(id),
+    FOREIGN KEY (logistic_companies_id) REFERENCES Logistic_companies(id),
+    FOREIGN KEY (status_id) REFERENCES Status(id)
 );
