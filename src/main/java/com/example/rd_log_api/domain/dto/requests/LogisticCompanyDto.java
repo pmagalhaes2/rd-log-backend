9f4910ef408984e93d6f140d19400187120adbad
pmagalhaes2 <paath.magalhaes2@gmail.com>
2024-06-04 11:05:04 -0300

    chore: add address field

--- src/main/java/com/example/rd_log_api/domain/dto/requests/LogisticCompanyDto.java
+++ src/main/java/com/example/rd_log_api/domain/dto/requests/LogisticCompanyDto.java
@@ -1,6 +1,5 @@
 package com.example.rd_log_api.domain.dto.requests;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,23 +11,15 @@ import java.sql.Time;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties
 public class LogisticCompanyDto {
-    public String address;
     private Long id;
-
     private String name;
-
     private String cnpj;
-
     private Time opening_hours;
-
     private Time closing_hours;
 
     private String phone_number;
-
     private String email;
 
-
-    public LogisticCompanyDto(Long id, String name, String cnpj, Time openingHours, Time closingHours, String phoneNumber, String email) {}
+    private AddressDto address;
 }
